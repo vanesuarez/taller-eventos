4e2c9bf8d22a5ce0d04cd9eb0f2193169c4e39ed
Vane Suarez <vanesuarez2301@gmail.com>
2023-08-07 00:59:02 -0300

    Se utilizo solutionpropagation

--- eventos.js
+++ eventos.js
@@ -1,4 +1,10 @@
 const dv = document.querySelector("div");
+const btn = document.querySelector("button")
+
+btn.addEventListener("click", (event) => {
+    event.stopPropagation();
+    alert("Hola!");
+} )
 
 dv.addEventListener ("click", () => {
     alert("Hola! Soy el Div");
